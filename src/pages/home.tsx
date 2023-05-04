10ac0aa7153e361efe72e5482db05b1f79a4b0ab
Sam <sambisselele@gmail.com>
2023-05-04 20:59:22 +0100

    Feat : set sidebar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { NavbarSimple } from "../components/SideBar";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div style={{ display: "flex" }}>
+      <NavbarSimple />
+      HomePage
+    </div>
+  );
 };
 
 export default HomePage;
